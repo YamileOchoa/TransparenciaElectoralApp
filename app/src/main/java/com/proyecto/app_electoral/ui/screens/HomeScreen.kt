8b0b6792256f290f58f9f44b4d68c9bb7e03059e
Yamile Ochoa <yamileochoa137@gmail.com>
2025-10-20 00:00:34 -0600

    Subiendo pantalla de inicio

--- app/src/main/java/com/proyecto/app_electoral/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/proyecto/app_electoral/ui/screens/HomeScreen.kt
@@ -1,25 +1,40 @@
 package com.proyecto.app_electoral.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.proyecto.app_electoral.R
+import kotlinx.coroutines.delay
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Scaffold { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(text = "Pantalla de Inicio")
-
-            Button(onClick = { navController.navigate("busqueda") }) {
-                Text("Ir a Búsqueda")
-            }
+fun HomeScreen(navController: NavHostController) {
+    LaunchedEffect(Unit) {
+        delay(1500L)
+        navController.navigate("busqueda") {
+            popUpTo("home") { inclusive = true }
         }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(350.dp)
+        )
     }
 }
