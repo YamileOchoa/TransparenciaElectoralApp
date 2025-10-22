b3d46ba891888962da3460b1b9c6d987e9a7ef8b
Yamile Ochoa <yamileochoa137@gmail.com>
2025-10-21 20:42:43 -0600

    Subiendo actualizacion de CompareScreen

--- app/src/main/java/com/proyecto/app_electoral/ui/screens/CompareScreen.kt
+++ app/src/main/java/com/proyecto/app_electoral/ui/screens/CompareScreen.kt
@@ -3,8 +3,6 @@ package com.proyecto.app_electoral.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
