4aa0f739696b0041a7bf728bc3b234eaeff61d3b
sesema98 <sergioserva14@gmail.com>
2025-11-06 19:38:19 -0500

    Eliminando carpetas y archivos antiguos del main

--- app/src/main/java/com/proyecto/app_electoral/data/model/HistorialCargo.kt
+++
@@ -1,9 +0,0 @@
-package com.proyecto.app_electoral.data.model
-
-data class HistorialCargo(
-    val cargo: String,
-    val institucion: String,
-    val fecha_inicio: String,
-    val fecha_fin: String,
-    val descripcion: String
-)
